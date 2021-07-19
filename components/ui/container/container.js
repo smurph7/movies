37a114ea15bd94a87c24baec1a841ce0366e199d
Sarah <sarahnmurphy93@gmail.com>
2021-07-19 11:35:39 +1000

    Add Container component

---
+++ components/ui/container/container.js
@@ -0,0 +1,20 @@
+import { styled } from '../../../stitches.config';
+
+export const Container = styled('div', {
+  mx: 'auto',
+  px: '$4',
+
+  variants: {
+    size: {
+      1: {
+        maxWidth: '300px'
+      },
+      2: {
+        maxWidth: '585px'
+      },
+      3: {
+        maxWidth: '865px'
+      }
+    }
+  }
+});
