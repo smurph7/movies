d38f979b16c97e5821e043fb44aa962fc47dd9a1
Sarah <sarahnmurphy93@gmail.com>
2021-07-21 17:06:25 +1000

    Add hook to get upcoming movies

---
+++ components/movies/hooks/use-upcoming-movies.js
@@ -0,0 +1,17 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+import { transformMovieData } from './transform-movie-data';
+
+export async function fetchUpcomingMovies() {
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/movie/upcoming?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&region=AU`
+  );
+  return data;
+}
+
+export function useUpcomingMovies() {
+  return useQuery(['upcoming'], fetchUpcomingMovies, {
+    select: data => transformMovieData(data)
+  });
+}
