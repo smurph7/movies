051c81a608461222b4fc2c1984f49100f9e4cc84
Sarah <sarahnmurphy93@gmail.com>
2021-07-21 17:57:47 +1000

    Refactor card styles

--- components/ui/card/card.js
+++ components/ui/card/card.js
@@ -2,10 +2,10 @@ import { styled } from '../../../stitches.config';
 
 export const Card = styled('div', {
   display: 'flex',
-  bg: 'white',
+  flexShrink: 0,
   flexDirection: 'column',
-  width: '100%',
-  height: 315,
+  width: '215px',
+  height: '315px',
   overflow: 'hidden',
   borderRadius: 10,
   boxShadow: '0 6px 10px -5px rgba(0, 0, 0, 0.2)',
