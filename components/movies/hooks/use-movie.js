7da4b968af97b88f5cbb58ddc6e0d9ede64995b6
Sarah <sarahnmurphy93@gmail.com>
2021-07-27 13:56:49 +1000

    Get movie details from API

---
+++ components/movies/hooks/use-movie.js
@@ -0,0 +1,28 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+function transformMovieData(data) {
+  return {
+    id: data.id,
+    title: data.title,
+    posterPath: data.poster_path,
+    voteAverage: data.vote_average,
+    overview: data.overview,
+    watchProviders: data['watch/providers'].results.AU
+  };
+}
+
+export async function fetchMovie({ queryKey }) {
+  const [, { id }] = queryKey;
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_TMDB_BASE_URL}/movie/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&append_to_response=watch/providers`
+  );
+  return data;
+}
+
+export function useMovie({ id }) {
+  return useQuery(['movie', { id }], fetchMovie, {
+    enabled: !!id,
+    select: data => transformMovieData(data)
+  });
+}
--- pages/movie/[slug].js
+++ pages/movie/[slug].js
@@ -2,9 +2,21 @@ import * as React from 'react';
 import { useRouter } from 'next/router';
 
 import { Layout } from '~/components/common';
+import { Text } from '~/components/ui';
+import { useMovie } from '~/components/movies/hooks';
 
 export default function Movie() {
   const router = useRouter();
   const { slug } = router.query;
-  return <Layout>{slug}</Layout>;
+
+  const splitSlug = slug?.split('-');
+  const id = splitSlug ? splitSlug[splitSlug?.length - 1] : undefined;
+
+  const movieQuery = useMovie({ id });
+
+  return (
+    <Layout>
+      <Text>{movieQuery.data?.title}</Text>
+    </Layout>
+  );
 }
