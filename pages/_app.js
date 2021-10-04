324155558c583ffd93073e661a2fb7b8b9a75476
Sarah <sarahnmurphy93@gmail.com>
2021-10-04 23:45:46 +1100

    Add auth0

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import { QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 import { queryClient } from '~/config/query-client';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </Hydrate>
-    </QueryClientProvider>
+    <UserProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Hydrate>
+      </QueryClientProvider>
+    </UserProvider>
   );
 }
