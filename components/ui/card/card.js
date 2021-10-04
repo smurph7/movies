4e1c98fb137a7a10f9151258180232830c0baaa2
Sarah <sarahnmurphy93@gmail.com>
2021-10-04 11:53:06 +1100

    Refactor card styles

--- components/ui/card/card.js
+++ components/ui/card/card.js
@@ -4,28 +4,12 @@ export const Card = styled('div', {
   display: 'flex',
   flexShrink: 0,
   flexDirection: 'column',
-  width: '215px',
-  height: '315px',
+  width: '233px',
+  height: '350px',
   overflow: 'hidden',
   borderRadius: 10,
   boxShadow: '0 6px 10px -5px rgba(0, 0, 0, 0.2)',
   transition: 'all 0.3s ease',
-  '@bp1': {
-    width: '175px',
-    height: '275px'
-  },
-  '@bp2': {
-    width: '200px',
-    height: '300px'
-  },
-  '@bp4': {
-    width: '200px',
-    height: '300px'
-  },
-  '@bp5': {
-    width: '215px',
-    height: '315px'
-  },
   variants: {
     bounceOnHover: {
       true: {
