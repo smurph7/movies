e591a9f01f35cf4b5bc38cbb2516caec5959c65a
Sarah <sarahnmurphy93@gmail.com>
2021-10-10 18:20:03 +1100

    Set US as fallback region for release date and certification

--- components/movies/hooks/use-release-dates.js
+++ components/movies/hooks/use-release-dates.js
@@ -3,17 +3,43 @@ import axios from 'axios';
 
 function transformReleaseDates(data) {
   const RELEASE_REGION = 'AU';
+  const FALLBACK_RELEASE_REGION = 'US';
   if (!data) {
     return;
   }
 
-  return data?.results
-    ?.filter(result => result.iso_3166_1 === RELEASE_REGION)[0]
-    ?.release_dates.map(result => ({
+  const regionResults = data?.results?.filter(
+    result => result.iso_3166_1 === RELEASE_REGION
+  )[0];
+
+  const fallbackRegionResults = data?.results?.filter(
+    result => result.iso_3166_1 === FALLBACK_RELEASE_REGION
+  )[0];
+
+  const regionData = regionResults?.release_dates.map(result => ({
+    certification: result.certification,
+    releaseDate: result.release_date
+  }))[0];
+
+  const fallbackRegionData = fallbackRegionResults?.release_dates.map(
+    result => ({
       certification: result.certification,
-      releaseDate: result.release_date,
-      region: RELEASE_REGION
-    }))[0];
+      releaseDate: result.release_date
+    })
+  )[0];
+
+  return {
+    certification:
+      regionData.certification !== ''
+        ? regionData.certification
+        : fallbackRegionData.certification,
+    releaseDate:
+      regionData.release_date !== ''
+        ? regionData.releaseDate
+        : fallbackRegionData.releaseDate,
+    region:
+      regionData.releaseDate !== '' ? RELEASE_REGION : FALLBACK_RELEASE_REGION
+  };
 }
 
 export async function fetchReleaseDates({ queryKey }) {
