e8689fc71193924082df73ba97cd811b41984254
Sarah <sarahnmurphy93@gmail.com>
2021-10-20 18:35:40 +1100

    Check if user exists before displaying favourites

--- pages/favourites/[page].js
+++ pages/favourites/[page].js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useRouter } from 'next/router';
+import { useUser } from '@auth0/nextjs-auth0';
 
 import {
   useFavouriteMovies,
@@ -7,13 +8,14 @@ import {
 } from '~/features/favourites/queries';
 import { Layout, Pagination } from '~/features/common/components';
 import { MovieTiles } from '~/features/movies/components';
-import { Flex } from '~/features/ui';
+import { Flex, Text } from '~/features/ui';
 import { useTotalPages } from '~/features/common/hooks/use-total-pages';
 
 export default function Favourites() {
+  const { user, isLoading: userIsLoading } = useUser();
   const router = useRouter();
   const page = router.query?.page;
-  const resultsPerPage = 20;
+  const resultsPerPage = 10;
 
   const favouritesQuery = useFavouriteMovies({ page, resultsPerPage });
   const { data: totalFavourites } = useFavouritesTotal();
@@ -32,28 +34,37 @@ export default function Favourites() {
 
   return (
     <Layout>
-      <Flex direction="column" gap={5}>
-        <MovieTiles
-          title="Favourites"
-          movies={favouritesQuery?.data?.results}
-          isLoading={favouritesQuery.isLoading || favouritesQuery.isIdle}
-        />
-        <Flex justify="center">
-          <Pagination
-            currentPage={page}
-            setCurrentPage={handlePageChange}
-            totalPages={totalPages}
-            edgePageCount={2}
-            middlePagesSiblingCount={2}
-          >
-            <Flex align="center" justify="center" gap={2}>
-              <Pagination.PrevButton />
-              <Pagination.PageButton />
-              <Pagination.NextButton />
+      {user || userIsLoading ? (
+        <Flex direction="column" gap={5}>
+          <MovieTiles
+            title="Favourites"
+            movies={favouritesQuery?.data?.results}
+            isLoading={favouritesQuery.isLoading || favouritesQuery.isIdle}
+          />
+          {favouritesQuery?.data?.results?.length > 0 && (
+            <Flex justify="center">
+              <Pagination
+                currentPage={page}
+                setCurrentPage={handlePageChange}
+                totalPages={totalPages}
+                edgePageCount={2}
+                middlePagesSiblingCount={2}
+              >
+                <Flex align="center" justify="center" gap={2}>
+                  <Pagination.PrevButton />
+                  <Pagination.PageButton />
+                  <Pagination.NextButton />
+                </Flex>
+              </Pagination>
             </Flex>
-          </Pagination>
+          )}
         </Flex>
-      </Flex>
+      ) : (
+        // TODO add sign in view
+        <Flex justify="center" css={{ p: '$8' }}>
+          <Text>Please log in to view your favourites</Text>
+        </Flex>
+      )}
     </Layout>
   );
 }
