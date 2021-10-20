8ad8df92da6fd1f3fe5a6243fc6f1db8ed23c741
Sarah <sarahnmurphy93@gmail.com>
2021-10-20 17:43:39 +1100

    Add pagination to favourites query

--- features/favourites/queries/use-favourite-movies.js
+++ features/favourites/queries/use-favourite-movies.js
@@ -7,32 +7,43 @@ import { useFavourites } from './use-favourites';
 import { transformMoviesData } from '~/features/movies/utils/transform-movie-data';
 
 export async function fetchMovie({ queryKey }) {
-  const [, { favourites }] = queryKey;
-  const requests = favourites.map(favourite => {
-    if (favourite === null) {
-      return;
-    }
-    return new Promise(resolve => {
-      moviesAxios
-        .get(`/movie/${favourite}`)
-        .then(({ data }) => {
-          resolve(data);
-        })
-        .catch(error => {
-          resolve(error);
-        });
+  const [, { favourites, page, resultsPerPage }] = queryKey;
+  const slice = [
+    Number(page) * resultsPerPage - resultsPerPage,
+    Number(page) * resultsPerPage
+  ];
+  const filteredFavourites = favourites.filter(favourite => favourite !== null);
+  const requests = filteredFavourites
+    .slice(slice[0], slice[1])
+    .map(favourite => {
+      if (favourite === null) {
+        return;
+      }
+      return new Promise(resolve => {
+        moviesAxios
+          .get(`/movie/${favourite}`)
+          .then(({ data }) => {
+            resolve(data);
+          })
+          .catch(error => {
+            resolve(error);
+          });
+      });
     });
-  });
 
   return Promise.all(requests)
     .then(data => data)
     .catch(err => console.error(err));
 }
 
-export function useFavouriteMovies() {
+export function useFavouriteMovies({ page = 1, resultsPerPage = 20 }) {
   const { data: favourites } = useFavourites();
-  return useQuery(['favouriteMovies', { favourites }], fetchMovie, {
-    enabled: !!favourites,
-    select: data => transformMoviesData({ results: data })
-  });
+  return useQuery(
+    ['favouriteMovies', { favourites, page, resultsPerPage }],
+    fetchMovie,
+    {
+      enabled: !!favourites,
+      select: data => transformMoviesData({ results: data })
+    }
+  );
 }
