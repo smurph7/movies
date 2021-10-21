9a54b1a14e3cc20ffc60f2804be99008b16ff7c7
Sarah <sarahnmurphy93@gmail.com>
2021-10-21 22:57:20 +1100

    Transform reviews data

---
+++ features/movies/components/movie-reviews/index.js
@@ -0,0 +1,1 @@
+export { MovieReviews } from './movie-reviews';
--- features/movies/queries/use-movie-reviews.js
+++ features/movies/queries/use-movie-reviews.js
@@ -2,6 +2,26 @@ import { useQuery } from 'react-query';
 
 import { moviesAxios } from '../../../api-client';
 
+function transformReviewData(data) {
+  return {
+    ...data,
+    results: data?.results?.map(result => ({
+      author: result?.author,
+      authorDetails: {
+        name: result?.author_details?.name,
+        username: result?.author_details?.username,
+        avatarPath: result?.author_details?.avatar_path,
+        rating: result?.author_details?.rating
+      },
+      content: result?.content,
+      createdAt: result?.created_at,
+      id: result?.id,
+      updatedAt: result?.updated_at,
+      url: result?.url
+    }))
+  };
+}
+
 export async function fetchMovieReviews({ queryKey }) {
   const [, { id }] = queryKey;
 
@@ -13,6 +33,6 @@ export async function fetchMovieReviews({ queryKey }) {
 export function useMovieReviews({ id }) {
   return useQuery(['reviews', { id }], fetchMovieReviews, {
     enabled: !!id,
-    select: data => data
+    select: data => transformReviewData(data)
   });
 }
