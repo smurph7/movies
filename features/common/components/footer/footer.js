5f8c163e374e713073309b68d3e5123e87a518d2
Sarah <sarahnmurphy93@gmail.com>
2021-10-23 01:16:41 +1100

    Add links to footer

--- features/common/components/footer/footer.js
+++ features/common/components/footer/footer.js
@@ -1,18 +1,25 @@
 import * as React from 'react';
 
-import { Text, Flex } from '~/features/ui';
+import { Text, Flex, Link } from '~/features/ui';
 
 export function Footer() {
   return (
     <Flex
       justify="center"
+      gap={2}
       css={{
         px: '$4',
         pt: '$8',
         pb: '$6'
       }}
     >
-      <Text color="gray">Powered by Me</Text>
+      <Link href="https://sarahmurphy.dev">
+        <Text color="gray">Powered by Me</Text>
+      </Link>
+      <Text color="gray">|</Text>
+      <Link href="https://github.com/smurph7/movies">
+        <Text color="gray">View Github</Text>
+      </Link>
     </Flex>
   );
 }
