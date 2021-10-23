068b79159282aa743e145ac7654282873f759902
Sarah <sarahnmurphy93@gmail.com>
2021-10-23 12:31:22 +1100

    Fix mobile movie card placeholder being too large

--- features/movies/components/movie-card/movie-card.js
+++ features/movies/components/movie-card/movie-card.js
@@ -23,7 +23,7 @@ export function MovieCard({ movie, isLoading, ...props }) {
 
   if (isLoading) {
     return (
-      <Card>
+      <Card {...props}>
         <Placeholder width="100%" height="100%" />
       </Card>
     );
